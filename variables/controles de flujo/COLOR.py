827a376a5f854a83e41f03324f7a7e676c6555a9
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-06-22 12:39:11 -0500

    Add files via upload

---
+++ variables/controles de flujo/COLOR.py
@@ -0,0 +1,14 @@
+#vocales=["a","e","i","o","u"]
+#for vocal in range(0,5):
+    print(vocales[numero])
+#for vocal in vocales:
+ #   print(vocal)
+
+
+colores=['azul','naranja','negro','rojo']
+
+for color in colores:
+    if color == 'rojo':
+        print("encontrado")
+        break
+    print(color)
