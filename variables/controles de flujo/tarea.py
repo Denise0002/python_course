f24314da41231575aa3c2adee0a3a02e7a1997e3
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-07-13 13:19:01 -0500

    Update tarea.py

--- variables/controles de flujo/tarea.py
+++ variables/controles de flujo/tarea.py
@@ -1,12 +1,13 @@
 # lista=[]
 
-#for i in range(5):
-   # palabra = input("ingresa una palabra: ")
+# for i in range(5):
+#    palabra = input("ingresa una palabra: ")
 #    lista.append(palabra)
-#for indice, palabra in enumerate(lista):
+# for indice, palabra in enumerate(lista):
 #    if palabra == "disco":
- #       print("disco, indice: ", indice)
+#        print("disco, indice: ", indice)
 
+import mensajes
 lista=[]
 indice=0
 palabra=''
@@ -17,4 +18,7 @@ for texto in range(0,len(lista)):
     if lista[texto] == 'disco':
         palabra=lista[texto]
         indice=texto
-print('el texto disco se encuentre en el indice', indice)
+if indice==0 and palabra=='':
+    print(mensajes.error('La palbra que buscas NO existe'))
+else:
+    print(mensajes.correcto(palabra,indice))
